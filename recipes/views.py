e27961a17c94a6fa767b72d4e5e171a24bf8343f
Judarist Fullstack <ivanbayiha17@gmail.com>
2025-11-05 13:42:44 +0100

    Page d'acceuil

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Recipe, Category
 
-# Create your views here.
+def home(request):
+    """Vue pour la page d'accueil"""
+    # Récupère les dernières recettes (limité à 8)
+    derniere_recettes = Recipe.objects.all().order_by('-date_creation')[:8]
+    
+    context = {
+        'derniere_recettes': derniere_recettes,
+    }
+    
+    return render(request, 'recipes/home.html', context)
